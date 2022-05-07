cc28f1f22770281c12b8628de32c4981e4a4bf38
Pedro Augusto Costa Anjos <pedrooaugustoo130@gmail.com>
2022-05-07 17:53:13 -0300

    Event handleBtnClearClick

--- js/controllers/form-controller.js
+++ js/controllers/form-controller.js
@@ -30,6 +30,7 @@ export function init() {
   state.errorNumber = document.querySelector('[data-error="number"]');
 
   state.inputNumber.addEventListener("change", handleInputNumberChange);
+  state.btnClear.addEventListener("click", handleBtnClearClick);
 }
 
 function handleInputNumberChange(event) {
@@ -38,6 +39,23 @@ function handleInputNumberChange(event) {
   } else {
     setFormError("number", "");
   }
+}
+
+function handleBtnClearClick(event) {
+  event.preventDefault();
+  clearForm();
+}
+
+function clearForm() {
+  state.inputCep.value = "";
+  state.inputCity.value = "";
+  state.inputNumber.value = "";
+  state.inputStreet.value = "";
+
+  setFormError("cep", "");
+  setFormError("number", "");
+
+  state.inputCep.focus();
 }
 
 function setFormError(key, value) {
