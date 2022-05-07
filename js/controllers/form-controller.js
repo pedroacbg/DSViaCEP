215158837c2fc178197757f6e3f1f69fe6e0b30d
Pedro Augusto Costa Anjos <pedrooaugustoo130@gmail.com>
2022-05-07 20:13:55 -0300

    Checking object Address before saving

--- js/controllers/form-controller.js
+++ js/controllers/form-controller.js
@@ -62,9 +62,21 @@ async function handleInputCepChange(event) {
   }
 }
 
-async function handleBtnSaveClick(event) {
+function handleBtnSaveClick(event) {
   event.preventDefault();
-  listController.addCard(state.address);
+
+  const errors = addressService.getErrors(state.address);
+
+  const keys = Object.keys(errors);
+
+  if (keys.length > 0) {
+    keys.forEach((key) => {
+      setFormError(key, errors[key]);
+    });
+  } else {
+    listController.addCard(state.address);
+    clearForm();
+  }
 }
 
 function handleInputNumberChange(event) {
@@ -88,6 +100,8 @@ function clearForm() {
 
   setFormError("cep", "");
   setFormError("number", "");
+
+  state.address = new Address();
 
   state.inputCep.focus();
 }
